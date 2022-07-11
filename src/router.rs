e64768ae1eda13d6a7928e3b26dfeef545e892d0
annieversary <annie@versary.town>
2022-07-11 17:49:49 +0100

    fix issues with default_layers

--- src/router.rs
+++ src/router.rs
@@ -32,25 +32,25 @@ impl RouterExtension for Router {
     }
 }
 
-/// adds the required layers, plus extra extensions
+/// adds the layers required by muxa, plus app-related extensions
 #[macro_export]
 macro_rules! default_layers {
     (
       builder: $builder:ident,
-      pool: $pool:ident,
-      config: $config:ident,
-      extensions: [ $($ext:ident),* $(,)? ],
+      pool: $pool:expr,
+      config: $config:expr,
+      extensions: [ $($ext:expr),* $(,)? ],
     ) => {
         tower::ServiceBuilder::new()
             .layer(tower_http::trace::TraceLayer::new_for_http())
             .layer(Extension($pool.clone()))
             .layer(Extension($config))
-            .layer(Extension(
-                muxa::sessions::DbSessionStore::new($pool).with_same_site(muxa::cookies::SameSite::Lax),
-            ))
             $(
                 .layer(Extension($ext))
             )*
+            .layer(Extension(
+                muxa::sessions::DbSessionStore::new($pool).with_same_site(muxa::cookies::SameSite::Lax),
+            ))
             .layer(middleware::from_fn(muxa::sessions::session_middleware))
             .layer(middleware::from_fn(
               <$builder as muxa::html::AssociatedMiddleware<_>>::Middleware::html_context_middleware,
